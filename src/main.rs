e7f216745d757fd3b5e87ef6485c81595bb0b78e
Sergio Ribera <sergioalejandroriberacosta@gmail.com>
2022-05-06 22:09:22 -0400

    Replace go to rust proyect

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+
+fn sum (a: i32, b: i32) -> i32 {
+    a + b
+}
+
+fn main() {
+    let result = sum(15, 26);
+    println!("The result of addition 15 + 26 = {}", result);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_sum() {
+        assert_eq!(sum(0, 0), 0);
+        assert_eq!(sum(1, 1), 2);
+        assert_eq!(sum(15, 26), 41);
+    }
+}
